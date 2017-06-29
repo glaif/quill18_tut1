201e6dc3d24e882ab7aa7d3e6c4427d5cd4d8a71
steve <smaldone@gmail.com>
2017-06-29 10:44:27 -0400

    Added camera angle change during zoom

--- Assets/CameraMouseController.cs
+++ Assets/CameraMouseController.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class CameraMouseController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    static readonly float MIN_HEIGHT = 4f;
+    static readonly float MAX_HEIGHT = 20f;
+    static readonly float MIN_ZOOM_INCR = 0.01f;
+    static readonly float LOW_ROTATE_ANGLE = 40f;
+    static readonly float HIGH_ROTATE_ANGLE = 90f;
+    static readonly float ROTATION_DAMPENER = 1.5f;
+            
+    // Use this for initialization
+    void Start () {
+        // change camera angle
+        Vector3 p = Camera.main.transform.position;
+
+        Camera.main.transform.rotation = Quaternion.Euler(
+            Mathf.Lerp(
+                LOW_ROTATE_ANGLE,
+                HIGH_ROTATE_ANGLE,
+                (p.y - MIN_HEIGHT) / (MAX_HEIGHT / ROTATION_DAMPENER)
+            ),
+            Camera.main.transform.rotation.eulerAngles.y,
+            Camera.main.transform.rotation.eulerAngles.z
+        );
+    }
 
     bool isDraggingCamera = false;
     Vector3 lastMousePosition;
@@ -51,7 +69,7 @@ public class CameraMouseController : MonoBehaviour {
 
         // Zoom in/out on mouse wheel movement
         float scrollAmount = Input.GetAxis("Mouse ScrollWheel");
-        if (Mathf.Abs(scrollAmount) > 0.01f) {
+        if (Mathf.Abs(scrollAmount) > MIN_ZOOM_INCR) {
             // Move camera towards hitPos
             Vector3 dir = hitPos - Camera.main.transform.position;
             Vector3 p = Camera.main.transform.position;
@@ -59,22 +77,35 @@ public class CameraMouseController : MonoBehaviour {
             //Debug.LogError(scrollAmount);
             //Debug.LogError(p.y);
             // Stop zooming when you hit boundary
-            if ((scrollAmount > 0) && (p.y > 4f)) {
+            if ((scrollAmount > 0) && (p.y > MIN_HEIGHT)) {
                 // zooming in above floor
                 Camera.main.transform.Translate(dir * scrollAmount, Space.World);
-            } else if ((scrollAmount < 0) && (p.y < 20f)) {
+            } else if ((scrollAmount < 0) && (p.y < MAX_HEIGHT)) {
                 // zooming out below ceiling
                 Camera.main.transform.Translate(dir * scrollAmount, Space.World);
             }
 
             p = Camera.main.transform.position;
 
-            if (p.y < 4f) {
-                p.y = 4f;
-            } else if (p.y > 20f) {
-                p.y = 20f;
+            if (p.y < MIN_HEIGHT) {
+                p.y = MIN_HEIGHT;
+            } else if (p.y > MAX_HEIGHT) {
+                p.y = MAX_HEIGHT;
             }
             Camera.main.transform.position = p;
+
+            // change camera angle
+            Camera.main.transform.rotation = Quaternion.Euler(
+                Mathf.Lerp(
+                    LOW_ROTATE_ANGLE, 
+                    HIGH_ROTATE_ANGLE, 
+                    (p.y-MIN_HEIGHT) / (MAX_HEIGHT / ROTATION_DAMPENER)
+                ),
+                Camera.main.transform.rotation.eulerAngles.y,
+                Camera.main.transform.rotation.eulerAngles.z
+            );
+            //float angle = Camera.main.transform.rotation.eulerAngles.x;
+            //Debug.LogError(angle);
         }
     }
 }
